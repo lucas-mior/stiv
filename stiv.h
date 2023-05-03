8e2546f781967022c97ad912f300d42a98a26568
lucas-mior <lucas.mior@tutamail.com>
2023-05-03 14:52:07 -0300

    complete type defs

--- stiv.h
+++ stiv.h
@@ -48,6 +48,7 @@ typedef unsigned char uchar;
 typedef unsigned short ushort;
 typedef unsigned int uint;
 typedef unsigned long ulong;
+typedef unsigned long long ulonglong;
 
 typedef int8_t int8;
 typedef int16_t int16;
