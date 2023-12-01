e449758fb32b65975c81c29ff91a0d764aa91fbb
lucas-mior <lucas.mior@tutamail.com>
2023-11-30 22:24:29 -0300

    main() comes first

--- main.c
+++ main.c
@@ -41,77 +41,8 @@ int exit_code = EXIT_FAILURE;
 static void main_usage(FILE *) __attribute__((noreturn));
 static void main_parse_args(Options *, int, char *[]);
 static void main_cache_name(Image *);
+static void image_reduce_size(Image *, double);
 
-static void image_reduce_size(Image *image, double new_width) {
-    FILE *cache_img;
-    char *XDG_CACHE_HOME = NULL;
-
-    const char *previewer = "previewer/stiv";
-    const char *jpg = "jpg";
-    char buffer[PATH_MAX];
-	int n;
-
-    double new_height;
-    if (new_width > MAX_IMG_WIDTH)
-        new_width = CACHE_IMG_WIDTH;
-
-    if ((XDG_CACHE_HOME = getenv("XDG_CACHE_HOME")) == NULL) {
-        fprintf(stderr, "XDG_CACHE_HOME is not set. Exiting...\n");
-        exit(EXIT_FAILURE);
-    }
-
-    n = snprintf(buffer, sizeof (buffer), 
-                "%s/%s/%s.%s", XDG_CACHE_HOME, previewer, image->cachename, jpg);
-	if (n < 0) {
-		fprintf(stderr, "Error printing cache name.\n");
-		exit(EXIT_FAILURE);
-	}
-    image->fullpath = util_strdup(buffer);
-
-    if ((cache_img = fopen(image->fullpath, "r"))) {
-        fclose(cache_img);
-        return;
-    }
-    if (errno == ENOENT) {
-        Imlib_Image imlib_image;
-        Imlib_Load_Error err;
-
-        double z = image->width / new_width;
-        new_height = round(((double) image->height / z));
-
-        imlib_image = imlib_load_image(image->basename);
-        imlib_context_set_image(imlib_image);
-        imlib_context_set_anti_alias(1);
-        imlib_image = imlib_create_cropped_scaled_image(
-                      0, 0, image->width, image->height, 
-                      (int) new_width, (int) new_height
-                      );
-        if (imlib_image == NULL)
-            goto dontcache;
-
-        imlib_context_set_image(imlib_image);
-
-        if (imlib_image_has_alpha()) {
-            imlib_image_set_format("png");
-        } else {
-            imlib_image_set_format("jpg");
-            imlib_image_attach_data_value("quality", NULL, 90, NULL);
-        }
-        imlib_save_image_with_error_return(image->fullpath, &err);
-        if (err)
-            goto dontcache;
-
-        imlib_free_image_and_decache();
-        return;
-    } else {
-        fprintf(stderr, "Error opening %s: %s\n",
-                        image->fullpath, strerror(errno));
-    }
-    dontcache:
-    free(image->fullpath);
-    image->fullpath = NULL;
-    return;
-}
 int main(int argc, char *argv[]) {
     Options options = {
         .w = 100, .h = HEIGHT_SHELL, .H = -1,
@@ -337,5 +268,76 @@ void main_cache_name(Image *image) {
 		exit(EXIT_FAILURE);
 	}
     image->cachename = util_strdup(buffer);
+    return;
+}
+
+void image_reduce_size(Image *image, double new_width) {
+    FILE *cache_img;
+    char *XDG_CACHE_HOME = NULL;
+
+    const char *previewer = "previewer/stiv";
+    const char *jpg = "jpg";
+    char buffer[PATH_MAX];
+	int n;
+
+    double new_height;
+    if (new_width > MAX_IMG_WIDTH)
+        new_width = CACHE_IMG_WIDTH;
+
+    if ((XDG_CACHE_HOME = getenv("XDG_CACHE_HOME")) == NULL) {
+        fprintf(stderr, "XDG_CACHE_HOME is not set. Exiting...\n");
+        exit(EXIT_FAILURE);
+    }
+
+    n = snprintf(buffer, sizeof (buffer), 
+                "%s/%s/%s.%s", XDG_CACHE_HOME, previewer, image->cachename, jpg);
+	if (n < 0) {
+		fprintf(stderr, "Error printing cache name.\n");
+		exit(EXIT_FAILURE);
+	}
+    image->fullpath = util_strdup(buffer);
+
+    if ((cache_img = fopen(image->fullpath, "r"))) {
+        fclose(cache_img);
+        return;
+    }
+    if (errno == ENOENT) {
+        Imlib_Image imlib_image;
+        Imlib_Load_Error err;
+
+        double z = image->width / new_width;
+        new_height = round(((double) image->height / z));
+
+        imlib_image = imlib_load_image(image->basename);
+        imlib_context_set_image(imlib_image);
+        imlib_context_set_anti_alias(1);
+        imlib_image = imlib_create_cropped_scaled_image(
+                      0, 0, image->width, image->height, 
+                      (int) new_width, (int) new_height
+                      );
+        if (imlib_image == NULL)
+            goto dontcache;
+
+        imlib_context_set_image(imlib_image);
+
+        if (imlib_image_has_alpha()) {
+            imlib_image_set_format("png");
+        } else {
+            imlib_image_set_format("jpg");
+            imlib_image_attach_data_value("quality", NULL, 90, NULL);
+        }
+        imlib_save_image_with_error_return(image->fullpath, &err);
+        if (err)
+            goto dontcache;
+
+        imlib_free_image_and_decache();
+        return;
+    } else {
+        fprintf(stderr, "Error opening %s: %s\n",
+                        image->fullpath, strerror(errno));
+    }
+    dontcache:
+    free(image->fullpath);
+    image->fullpath = NULL;
     return;
 }
