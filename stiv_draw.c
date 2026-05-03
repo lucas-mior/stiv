845144ac6f56004bbe4ad04cda78298c0cdc54ed
lucas-mior <lucas.mior@tutamail.com>
2026-05-02 21:58:06 -0300

    wip

--- stiv_draw.c
+++ stiv_draw.c
@@ -178,8 +178,8 @@ main(int argc, char *argv[]) {
     }
 
     if (print_dimensions) {
-        printf("\033[01;31m%d\033[0;mx\033[01;31m%d\033[0;m\n", image.width,
-               image.height);
+        printf("\033[01;31m%d\033[0;mx\033[01;31m%d\033[0;m\n",
+               image.width, image.height);
     }
 
     if (argc >= 6) {
