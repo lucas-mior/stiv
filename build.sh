4c3db3c491da331cf4cb61ee95a50a0f7088ee80
lucas-mior <lucas.mior@tutamail.com>
2026-07-24 17:06:08 -0300

    update build.sh

--- build.sh
+++ build.sh
@@ -68,7 +68,12 @@ case "$target" in
     ;;
 "check")
     CC=gcc CFLAGS="-fanalyzer" ./build.sh
-    scan-build --view -analyze-headers --status-bugs ./build.sh
+
+    CFLAGS="--analyze -Xanalyzer -analyzer-output=text"
+    CFLAGS="$CFLAGS -Xanalyzer -analyzer-werror"
+    CFLAGS="$CFLAGS -Xanalyzer -analyzer-opt-analyze-headers"
+    CFLAGS="$CFLAGS -Wno-unused-command-line-argument"
+    CC=clang CFLAGS="$CFLAGS" ./build.sh
     exit
     ;;
 "build"|"debug"|"benchmark")
