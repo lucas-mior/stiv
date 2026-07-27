f2caff6679565889459e712bbca0ee43cdc611f3
lucas-mior <lucas.mior@tutamail.com>
2026-07-26 21:02:46 -0300

    wip

--- build.sh
+++ build.sh
@@ -36,6 +36,8 @@ if [ $CC = "clang" ]; then
     CFLAGS="$CFLAGS -Wno-float-equal"
     CFLAGS="$CFLAGS -Wno-cast-qual"
     CFLAGS="$CFLAGS -Wno-cast-function-type-strict"
+    CFLAGS="$CFLAGS -Wno-assign-enum"
+    CFLAGS="$CFLAGS -Wno-implicit-int-enum-cast"
 fi
 
 if [ "$target" = "debug" ]; then
